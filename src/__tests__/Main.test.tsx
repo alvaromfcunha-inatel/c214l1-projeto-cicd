d28c169ce1afd247dae436ce899579970030042e
Alvaro <alvaro99.cunha@gmail.com>
2022-11-25 21:14:09 -0300

    added main component render test

--- src/__tests__/Main.test.tsx
+++ src/__tests__/Main.test.tsx
@@ -1,5 +1,12 @@
+import { render, screen } from '@testing-library/react';
+
 import Main from '../components/Main/Main';
 
-test('Renders main page correctly', () => {
-  expect(true).toBeTruthy();
-});
+test('renders Main component successfuly', async () => {
+  render(<Main />)
+
+  const expectedHeader = 'Criar Aprensentação'
+  const header = await screen.findByText(expectedHeader)
+
+  expect(header).toBeTruthy()
+})
