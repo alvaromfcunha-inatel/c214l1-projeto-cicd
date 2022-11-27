63fd4d4d9e44a57d383d454e4151ccee72f5148e
Alvaro <alvaro99.cunha@gmail.com>
2022-11-27 14:46:19 -0300

    added more test cases

--- src/__tests__/Main.test.tsx
+++ src/__tests__/Main.test.tsx
@@ -1,12 +1,61 @@
 import { render, screen } from '@testing-library/react';
+import user from '@testing-library/user-event';
 
 import Main from '../components/Main/Main';
 
 test('renders Main component successfuly', async () => {
-  render(<Main />)
+  const { container } = render(<Main />)
 
   const expectedHeader = 'Criar Aprensentação'
-  const header = await screen.findByText(expectedHeader)
+  const header = container.querySelector('h1')
 
-  expect(header).toBeTruthy()
+  expect(header?.textContent).toBeTruthy()
+  expect(header?.textContent).toBe(expectedHeader)
+})
+
+test('empty slide list inital state', async () => {
+  const { container } = render(<Main />)
+
+  const slideList = container.querySelector('.card-item')
+
+  expect(slideList).toBe(null)
+})
+
+test('add one slide successfuly', async () => {
+  const { container } = render(<Main />)
+
+  const initialSlideList = container.querySelector('.card-item')
+  const slideAddButton = await screen.findByText('Add')
+
+  expect(initialSlideList).toBe(null)
+
+  await user.click(slideAddButton)
+
+  const slide = container.querySelector('#slide-1')
+
+  expect(slide).toBeTruthy()
+})
+
+test('add many slides successfuly', async () => {
+  const { container } = render(<Main />)
+
+  const initialSlideList = container.querySelector('.card-item')
+  const slideAddButton = await screen.findByText('Add')
+
+  expect(initialSlideList).toBe(null)
+
+  await user.click(slideAddButton)
+  await user.click(slideAddButton)
+  await user.click(slideAddButton)
+  await user.click(slideAddButton)
+
+  const slide1 = container.querySelector('#slide-1')
+  const slide2 = container.querySelector('#slide-2')
+  const slide3 = container.querySelector('#slide-3')
+  const slide4 = container.querySelector('#slide-4')
+
+  expect(slide1).toBeTruthy()
+  expect(slide2).toBeTruthy()
+  expect(slide3).toBeTruthy()
+  expect(slide4).toBeTruthy()
 })
